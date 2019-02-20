a746d85f5562984b28f188200e5a2360b25f873f
ваше имя < >
2019-02-21 00:52:12 +0600

    Добавлен компоненты

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -1,2 +1,26 @@
 import React, { Component } from 'react';
 import './AddMovie.css';
+
+
+function AddMovie(props) {
+
+    return (
+        <div className={"addtaskform"}>
+            <form className={'d-inline'}>
+                <input
+                    type="text"
+                    value={props.movie.name}
+                    onChange={props.onChangeInput}
+                />
+            </form>
+            <div className={'d-inline'}>
+                <button onClick={props.onAddMovie}>
+                    Add
+                </button>
+            </div>
+        </div>
+    )
+}
+
+
+export default AddMovie
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,2 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './MovieList.css';
+
+
+class MovieList extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Post] ShouldUpdate');
+        return nextProps.name !== this.props.name
+    }
+
+    render() {
+        return (
+            <div className={"task row mt-3 border border-primary rounded w-50 ml-auto mr-auto p-2"}>
+                <div className={'col'}>
+                    <input type="text" name="name" value={this.props.name} onChange={this.props.onChangeMovie}/>
+
+                </div>
+                <div className={'row col'}>
+
+                    <div className={'col'}>
+                        <button onClick={this.props.onDeleteMovie}>
+                            <i className="far fa-trash-alt"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default MovieList
